b1669d30ac027241a6911f8f0374cffa7f2995ff
bodascaluca <bodasca.luca@gmail.com>
2022-04-21 17:31:16 +0200

    completed pari-dispari excersize

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -6,8 +6,8 @@
 // Dichiariamo chi ha vinto.
 
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-const evenOrOdd = prompt(`Pari o dispari?`);
-console.log(evenOrOdd);
+const askEvenOrOdd = prompt(`Pari o dispari?`);
+console.log(askEvenOrOdd);
 
 const randomUserNumber = parseInt(prompt(`Dimmi un numero tra 1 e 5`));
 console.log(randomUserNumber);
@@ -17,8 +17,25 @@ const randomPcNUmber = randomNumber();
 console.log(randomPcNUmber);
 
 // Sommiamo i due numeri
-const sumNumber = (randomUserNumbe +  randomPcNUmbe );
+const sumNumber = randomUserNumber + randomPcNUmber;
 console.log("La somma è",sumNumber);
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+const resultEvenOrOdd = evenOrOdd(sumNumber);
+console.log("La somma è" ,resultEvenOrOdd);
+
+// Dichiariamo chi ha vinto.
+let  endResult = "Hai perso";
+if (askEvenOrOdd === resultEvenOrOdd){
+    endResult = "Hai vinto"
+} 
+console.log("Quindi",endResult);
+
+
+
+
+
+
 
 
 
@@ -26,5 +43,17 @@ console.log("La somma è",sumNumber);
 function randomNumber(){
     const result = Math.floor(Math.random() * 5) + 1;
     // console.log(result);
+    return result;
+}
+
+//Funzione evenOrOdd
+function evenOrOdd (sumNumber){
+    if (sumNumber % 2 === 0){
+        result = "pari"; 
+        console.log(result);
+    }else if (sumNumber % 2 === 1){
+        result = "dispari";
+        console.log(result);
+    }
     return result;
 }
